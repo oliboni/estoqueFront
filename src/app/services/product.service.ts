65981edc2cfcbe4b2e16270d22476c96ed40bd49
Rodrigo Oliboni <rodrigo_oliboni@hotmail.com>
2019-06-18 19:22:34 -0300

    update models

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { Product} from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  constructor(private _http: HttpClient) { }
+
+  getAll(): Observable<Product[]>{
+    return this._http.get<Product[]>('http://localhost:3000/products');
+  }
+
+  get(id: number): Observable<Product>{
+    return this._http.get<Product>('http://localhost:3000/products' + id);
+  }
 }
